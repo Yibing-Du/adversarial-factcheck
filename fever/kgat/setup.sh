9a211cbb73c8e6ee8c5a361a0a1d7baa8cce5b33
Yibing-Du <yibingdu@stanford.edu>
2022-02-18 08:03:08 -0800

    Update setup.sh

--- fever/kgat/setup.sh
+++ fever/kgat/setup.sh
@@ -6,5 +6,6 @@ wget https://huggingface.co/roberta-large/resolve/main/vocab.json
 cd checkpoint
 sh download.sh
 
+mkdir output
 mkdir checkpoint
 # Download kgat_pred_model.best.pt: https://drive.google.com/file/d/1RlFUVr_loIL0nJenhrtEAzqeBYfMrJ5S/view?usp=sharing
